982d530fb4d9239bd625449665fd254f4b8cdd12
SungJinJang96 <bbb0649@nate.com>
2022-03-03 16:26:21 +0900

    3/3

---
+++ Algorithm/src/백준/_3460번_이진수.java
@@ -0,0 +1,25 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class _3460번_이진수 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		
+		int t = sc.nextInt();
+		
+		
+		for(int i =0 ; i<t ; i++) {
+			int n = sc.nextInt();
+			String bin = Integer.toBinaryString(n);
+			for(int j = 0 ; j<bin.length(); j++ ) {
+				if(bin.charAt(bin.length()-j-1)=='1') {
+					System.out.print(j + " ");
+				}
+			}
+		}
+		
+	}
+
+}
