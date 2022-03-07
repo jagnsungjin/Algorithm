777498a3838f0c12c69557773e8ea48da2007f38
SungJinJang96 <sjphone95@naver.com>
2022-03-07 13:22:35 +0900

    3/7

---
+++ Algorithm/src/백준/_1789번_수들의합_완료.java
@@ -0,0 +1,31 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _1789번_수들의합_완료 {
+	public static void main(String args[]) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        StringTokenizer st = null;
+	        
+	        st = new StringTokenizer(br.readLine());
+	      	long s = Long.parseLong(st.nextToken()); 
+	      	
+	      	long count=1;
+	      	long sum=1;
+	      	
+	      	while(sum<s){
+	      		count++;
+	      		sum+=count;
+	      	}
+	      	if(sum>s){
+	  			count--;
+	  		}
+
+	      	
+	      	System.out.println(count);
+		
+	}
+}
