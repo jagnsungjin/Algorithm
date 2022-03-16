dee6b2b6267cbeec0632661fa18c10955d48265a
SungJinJang96 <sjphone95@naver.com>
2022-03-16 17:32:08 +0900

    3/16

--- Algorithm/src/백준/_1292번_쉽게푸는문제_완료.java
+++ Algorithm/src/백준/_1292번_쉽게푸는문제_완료.java
@@ -9,23 +9,21 @@ public class _1292번_쉽게푸는문제_완료 {
 		int su1 = sc.nextInt(); 
 		int su2 = sc.nextInt();
 		
-		int result = 0 ;
+		int result = 0 ;		// 누적 값 
 		
-		for(int i = su1 ; i <= su2; i++) {
-			result += hamsu(i);
+		for(int i = su1 ; i <= su2; i++) {	//입력 값 ~ 입력 값 까지 반복
+			result += hamsu(i);				//누적 값 
 		}
 		
 		System.out.println(result);
 	
 		
-
-		
 	}
 	
 	private static int hamsu(int su1) {
 		
-		double a = 1.0;
-		double div = 1.0;
+		double a = 1.0;			//나눈 값 을 비교할 값
+		double div = 1.0;		//나눌 값 
 		
 		for(double i = 1; i <=  su1 ; i++) {
 			if(i/div > a) {
@@ -34,7 +32,7 @@ public class _1292번_쉽게푸는문제_완료 {
 			}
 		}
 		
-		int res = (int) (div/1);
+		int res = (int) (div/1);	
 		
 		return res;
 	}
